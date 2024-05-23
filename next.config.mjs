cf8bfea2027ee3e842001a2850d5c24ace26f885
kajal <kumarinaina9876@gmail.com>
2024-05-23 14:31:50 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+
+import bundleAnalyzer from '@next/bundle-analyzer';
+import webpack from 'webpack'; // Import webpack module
+
+const nextConfig = {
+  // Adjust webpack options
+  webpack: (config) => {
+    // Remove files matching /__tests__/ from the bundle
+    config.plugins.push(new webpack.IgnorePlugin({
+      resourceRegExp: /\/__tests__\//,
+    }));
+    return config;
+  },
+
+  // Bundle Analyzer configuration
+  ...bundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+  }),
+
+  // Image optimization
+  images: {
+    domains: ['example.com'],
+  },
+};
 
 export default nextConfig;
